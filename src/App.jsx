6864cfb31bd192be7a550c21e1595a074d8744fa
MiJouHsieh <mijouhsieh@gmail.com>
2024-10-01 10:37:22 +0800

    feat(app): add dark mode toggle and update routing configuration
    - Implemented a dark mode toggle with `useState` hook.
    - Updated routes in `App.jsx` to include signup, signin, and user profile pages.
    - Passed `toggleTheme` and `isDarkMode` as props to `HomePage`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,61 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage, LoginPage, SignUpPage } from "./pages";
+import { HomePage, SignInPage, SignUpPage, UserProfilePage } from "src/pages";
+
+import { useState } from "react";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { lightTheme, darkTheme } from "src/theme/theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+  }
+`;
+
+const Container = styled.div`
+  margin: 0 auto;
+  height: 932px;
+  width: 100%;
+  min-width: 360px;
+
+  @media screen and (min-width: 376px) {
+    max-width: 573px;
+    padding: 0;
+  }
+
+  @media screen and (min-width: 769px) {
+    max-width: 1440px;
+  }
+`;
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => setIsDarkMode(!isDarkMode);
+
   return (
-    <>
-      <div>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Container>
         <BrowserRouter>
           <Routes>
             <Route path="signup" element={<SignUpPage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="*" element={<HomePage />} />
+            <Route path="signin" element={<SignInPage />} />
+            <Route
+              path="*"
+              element={
+                <HomePage toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+              }
+            />
+            <Route
+              path="user-profile-page"
+              element={<UserProfilePage />}
+              toggleTheme={toggleTheme}
+              isDarkMode={isDarkMode}
+            />
           </Routes>
         </BrowserRouter>
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   );
 }
 
